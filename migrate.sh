272a1eb078f29fa316dd1ace834309feb51a4a1f
MeftunH <maftunhashimli@gmail.com>
2024-01-18 23:40:20 +0300

    add migrate tool

---
+++ migrate.sh
@@ -0,0 +1,33 @@
+os400=false
+case "`uname`" in
+OS400*) os400=true;;
+esac
+
+PRG="$0"
+
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+PRGDIR=`dirname "$PRG"`
+EXECUTABLE=tool-wrapper.sh
+
+if $os400; then
+
+  eval
+else
+  if [ ! -x "$PRGDIR"/"$EXECUTABLE" ]; then
+    echo "Cannot find $PRGDIR/$EXECUTABLE"
+    echo "The file is absent or does not have execute permission"
+    echo "This file is needed to run this program"
+    exit 1
+  fi
+fi
+
+exec "$PRGDIR"/"$EXECUTABLE" -server org.apache.tomcat.jakartaee.MigrationCLI "$@"
